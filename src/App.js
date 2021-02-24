ab3d7255d8a91e923621f5b82ff45b968da2df53
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-02-24 12:20:49 +0530

    another deploy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>This is deploy check</h2>
+        <p>Adding another deploy</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
